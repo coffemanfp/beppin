261e8a3e46f0c8fd90697732188274724ead994d
CoffeManFP <coffemanfp@gmail.com>
2020-08-27 09:58:58 -0400

    Added helpers ExistsUser and ExistsLanguage. Grouped helpers

--- handlers/helpers_test.go
+++ handlers/helpers_test.go
@@ -5,18 +5,66 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/coffemanfp/beppin-server/config"
 	"github.com/coffemanfp/beppin-server/database"
+	dbm "github.com/coffemanfp/beppin-server/database/models"
 	errs "github.com/coffemanfp/beppin-server/errors"
 	"github.com/coffemanfp/beppin-server/handlers"
+	"github.com/coffemanfp/beppin-server/helpers"
 	"github.com/coffemanfp/beppin-server/models"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/stretchr/testify/assert"
 )
 
-var token string = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJsYW5ndWFnZSI6ImVzLUVTIiwidXNlcm5hbWUiOiJjb2ZmZW1hbmZwIiwidGhlbWUiOiJsaWdodCJ9fQ.GJcykxeN4yfE7CVi1xu4zVYstPgODCuNtrgq4T11gA4"
+var token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJsYW5ndWFnZSI6ImVzLUVTIiwidXNlcm5hbWUiOiJjb2ZmZW1hbmZwIiwidGhlbWUiOiJsaWdodCJ9fQ.GJcykxeN4yfE7CVi1xu4zVYstPgODCuNtrgq4T11gA4"
+var exampleTime = time.Now()
+
+var exampleLanguage = models.Language{
+	Code:   "es-ES",
+	Status: "available",
+}
+
+var exampleUser = models.User{
+	Language: "es-ES",
+	Name:     "Franklin",
+	Username: "coffemanfp",
+	Password: "24061502",
+	LastName: "Peñaranda",
+	Theme:    "dark",
+	Birthday: &exampleTime,
+}
+
+var exampleProducts = models.Products{
+	models.Product{
+		UserID:      1,
+		Name:        "Product name",
+		Description: "Product description",
+		Categories: []string{
+			"Food",
+		},
+	},
+	models.Product{
+		UserID:      1,
+		Name:        "Product name 2",
+		Description: "Product description 2",
+		Categories: []string{
+			"Tech",
+		},
+	},
+	models.Product{
+		UserID:      1,
+		Name:        "Product name 3",
+		Description: "Product description 3",
+		Categories: []string{
+			"Clothes",
+		},
+	},
+}
+
+// Server helpers
 
 func assertInvalidParam(t *testing.T, param string, err error) {
 	t.Helper()
@@ -38,6 +86,8 @@ func assertInternalServerError(t *testing.T, err error) {
 }
 
 func assertCreated(t *testing.T, rec *httptest.ResponseRecorder) {
+	t.Helper()
+
 	assert.Equal(t, http.StatusCreated, rec.Code)
 
 	var m models.ResponseMessage
@@ -46,6 +96,8 @@ func assertCreated(t *testing.T, rec *httptest.ResponseRecorder) {
 }
 
 func setJWTMiddleware(t *testing.T, e *echo.Echo) {
+	t.Helper()
+
 	jwtConfig := middleware.JWTConfig{
 		Claims:      &models.Claim{},
 		SigningKey:  []byte(config.GetSettings().SecretKey),
@@ -55,16 +107,69 @@ func setJWTMiddleware(t *testing.T, e *echo.Echo) {
 	e.Use(middleware.JWTWithConfig(jwtConfig))
 }
 
+func setAuthorizationRequest(t *testing.T, req *http.Request, token string) {
+	t.Helper()
+
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	req.Header.Set(echo.HeaderAuthorization, token)
+}
+
+// Database helpers
+
 func setStorage(t *testing.T) {
+	t.Helper()
+
 	storage, err := database.NewDefault()
 	assert.Nil(t, err)
 	assert.NotNil(t, storage)
 	handlers.Storage = storage
 }
 
-func setAuthorizationRequest(t *testing.T, req *http.Request, token string) {
+func insertUser(t *testing.T, user models.User) {
 	t.Helper()
 
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	req.Header.Set(echo.HeaderAuthorization, token)
+	userDB, err := helpers.ParseModelToDBModel(user)
+	assert.Nil(t, err)
+
+	assert.Nil(t, handlers.Storage.CreateUser(userDB.(dbm.User)))
+}
+
+func existsUser(t *testing.T, user models.User) (exists bool) {
+	t.Helper()
+
+	userDB, err := helpers.ParseModelToDBModel(user)
+	assert.Nil(t, err)
+
+	exists, err = handlers.Storage.ExistsUser(userDB.(dbm.User))
+	assert.Nil(t, err)
+	return
+}
+
+func insertLanguage(t *testing.T, language models.Language) {
+	t.Helper()
+
+	languageDB, err := helpers.ParseModelToDBModel(language)
+	assert.Nil(t, err)
+
+	assert.Nil(t, handlers.Storage.CreateLanguage(languageDB.(dbm.Language)))
+}
+
+func existsLanguage(t *testing.T, language models.Language) (exists bool) {
+	t.Helper()
+
+	languageDB, err := helpers.ParseModelToDBModel(language)
+	assert.Nil(t, err)
+
+	exists, err = handlers.Storage.ExistsLanguage(languageDB.(dbm.Language))
+	assert.Nil(t, err)
+	return
+}
+
+func insertProduct(t *testing.T, product models.Product) {
+	t.Helper()
+
+	productDB, err := helpers.ParseModelToDBModel(product)
+	assert.Nil(t, err)
+
+	assert.Nil(t, handlers.Storage.CreateProduct(productDB.(dbm.Product)))
 }
