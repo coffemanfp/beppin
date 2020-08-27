2dd22f631fdf4f826b319019ae5ead227b033656
CoffeManFP <coffemanfp@gmail.com>
2020-08-27 11:03:04 -0400

    Advanced the CreateProduct tests

--- handlers/create_product_test.go
+++ handlers/create_product_test.go
@@ -6,8 +6,10 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/coffemanfp/beppin-server/handlers"
+	"github.com/coffemanfp/beppin-server/models"
 	"github.com/labstack/echo"
 	"github.com/stretchr/testify/assert"
 )
@@ -41,4 +43,58 @@ func TestCreateProduct(t *testing.T) {
 	e.ServeHTTP(rec, req)
 
 	assertCreated(t, rec)
+}
+
+func TestFailedCreateProduct(t *testing.T) {
+	tests := []struct {
+		Name string
+		Body interface{}
+	}{
+		{
+			Name: "invalid_body",
+			Body: "alksdlkadjs",
+		},
+		{
+			Name: "empty_product",
+			Body: models.Product{},
+		},
+		{
+			Name: "not_existent_user",
+			Body: models.Product{
+				UserID:      time.Now().Unix(),
+				Name:        exampleProducts[0].Name,
+				Description: exampleProducts[0].Description,
+				Categories:  exampleProducts[0].Categories,
+			},
+		},
+	}
+
+	for _, ts := range tests {
+		t.Run(ts.Name, func(t *testing.T) {
+			// Setup server
+			e := echo.New()
+			e.Logger.Debug()
+
+			setJWTMiddleware(t, e)
+
+			e.POST("/", handlers.CreateProduct)
+
+			bodyJSON, err := json.Marshal(ts.Body)
+			assert.Nil(t, err)
+
+			// Now the request
+			req := httptest.NewRequest(http.MethodPost, "/", bytes.NewReader(bodyJSON))
+
+			setAuthorizationRequest(t, req, token)
+
+			rec := httptest.NewRecorder()
+			e.ServeHTTP(rec, req)
+
+			if ts.Name == "not_existent_user" {
+				assertNotExistent(t, rec, "user")
+			} else {
+				assertInvalidBody(t, rec)
+			}
+		})
+	}
 }
