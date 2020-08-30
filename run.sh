e725421c123ef9cd8a37491c53f31790c6e078b1
CoffeManFP <coffemanfp@gmail.com>
2020-08-30 11:00:07 -0400

    Removed unnecesary code

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go build -o ./bin/beppin-server && ./bin/beppin-server --read-config-file=false --read-env-vars=false
+go build -o ./bin/beppin-server && ./bin/beppin-server --read-env-vars=false
