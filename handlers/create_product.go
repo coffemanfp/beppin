e6cab1255c3006819f937a896d3d3a1bb739e9ce
Franklin Peñaranda <coffemanfp@gmail.com>
2020-11-22 11:51:51 -0400

    Checked product images when it's created

--- handlers/create_product.go
+++ handlers/create_product.go
@@ -7,6 +7,7 @@ import (
 
 	errs "github.com/coffemanfp/beppin/errors"
 	"github.com/coffemanfp/beppin/models"
+	"github.com/coffemanfp/beppin/utils"
 	"github.com/labstack/echo"
 )
 
@@ -25,6 +26,41 @@ func CreateProduct(c echo.Context) (err error) {
 		m.Error = errs.ErrInvalidBody
 
 		return echo.NewHTTPError(http.StatusBadRequest, m)
+	}
+
+	// Check if exists the files
+	if len(product.Images) != 0 {
+		for _, image := range product.Images {
+			// Get old file info
+			oldFile, err := Storage.GetFile(models.File{ID: int64(image.ID)})
+			if err != nil {
+				if errors.Is(err, errs.ErrNotExistentObject) {
+					m.Error = fmt.Sprintf("%v: file", errs.ErrNotExistentObject)
+
+					return echo.NewHTTPError(http.StatusNotFound, m)
+				}
+				c.Logger().Error(err)
+				m.Error = http.StatusText(http.StatusInternalServerError)
+
+				return echo.NewHTTPError(http.StatusInternalServerError, m)
+			}
+
+			// Check if the file exists in the filesystem
+			exists, err := utils.ExistsFile(oldFile.Path)
+			if err != nil {
+				c.Logger().Error(err)
+				m.Error = http.StatusText(http.StatusInternalServerError)
+
+				return echo.NewHTTPError(http.StatusInternalServerError, m)
+			}
+
+			if !exists {
+				m.Error = fmt.Sprintf("%v: file", errs.ErrNotExistentObject)
+
+				return echo.NewHTTPError(http.StatusNotFound, m)
+			}
+
+		}
 	}
 
 	createdProduct, err := Storage.CreateProduct(product)
